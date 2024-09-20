91c06fd28890453da1309d8f0b74fcb72e86de2d
Annay Paul <paulannay14@gmail.com>
2024-09-20 15:44:17 +0600

    Create a helloWorld component - capable of taking input and dynamically updating text

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 // https://trailhead.salesforce.com/content/learn/projects/quick-start-lightning-web-components/create-a-hello-world-lightning-web-component?trail_id=build-lightning-web-components
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    greeting = "World";
+    changeHandler(event){
+        this.greeting = event.target.value;
+    }
+}
