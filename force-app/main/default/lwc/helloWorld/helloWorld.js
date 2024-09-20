1bda39e04335cec3081f2f32ba49e621829969a6
Annay Paul <paulannay14@gmail.com>
2024-09-20 14:56:06 +0600

    Initialize a "helloWorld" component for Trailhead

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,3 @@
 import { LightningElement } from 'lwc';
-
+// https://trailhead.salesforce.com/content/learn/projects/quick-start-lightning-web-components/create-a-hello-world-lightning-web-component?trail_id=build-lightning-web-components
 export default class HelloWorld extends LightningElement {}
