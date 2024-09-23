19a9da0f2b163e20e1513681e0ec72ef706f6a03
Annay Paul <paulannay14@gmail.com>
2024-09-23 19:11:57 +0600

    Lesson 2: Parent to Child communication using api method

--- force-app/main/default/lwc/child/child.js
+++ force-app/main/default/lwc/child/child.js
@@ -1,25 +1,9 @@
-import { LightningElement, api } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
 export default class Child extends LightningElement {
-    constructor(){
-        super();
-        console.log('Child Constructor');
-    }
-    @api percentage;
+    timestamp = new Date();
 
-    get design(){
-        return `background-color: aqua; min-height: 50px; width: ${this.percentage}%; border:1px solid black `;
-    }
-
-    connectedCallback(){
-        console.log('Child Connected Callback');
-    }
-
-    disconnectedCallback(){
-        console.log('Child Disconnected Callback');
-    }
-
-    renderedCallback(){
-        console.log('Child Rendered Callback');
+    @api refresh(){
+        this.timestamp = new Date();
     }
 }
--- force-app/main/default/lwc/parent/parent.js
+++ force-app/main/default/lwc/parent/parent.js
@@ -1,23 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class Parent extends LightningElement {
-    percentage=20;
-    handleChange(event){
-        this.percentage = event.target.value;
-    }
-
-    constructor(){
-        super();
-        console.log('Parent');
-    }
-    connectedCallback(){
-        console.log('Parent Connected');
-    }
-    disconnectedCallback(){
-        console.log('Parent Disconnected');
-    }
-
-    renderedCallback(){
-        console.log('Parent Rendered')
+    handleClick(){
+        this.template.querySelector('c-child').refresh();
     }
 }
