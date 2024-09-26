251085709f625c5a6f66d844130f554ed62df794
Annay Paul <paulannay14@gmail.com>
2024-09-26 18:31:40 +0600

    [Proto-1] Dynamic Dropdown (STEP 2)

--- force-app/main/default/lwc/scratch/scratch.js
+++ force-app/main/default/lwc/scratch/scratch.js
@@ -1,18 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Scratch extends LightningElement {
     selectedOption = "";
+    inputText = "";
+
+    @track
+    optionsArray = [
+        {label: 'Option 1', value: 'Option 1'}, 
+        {label: 'Option 2', value: 'Option 2'}, 
+        {label: 'Option 3', value: 'Option 3'},
+        {label: 'Option 4', value: 'Option 4'},
+    ];
+
+    handleSubmit(event){
+        event.preventDefault();
+        this.inputText = this.refs.searchInput.value;
+        this.optionsArray.push({label: this.inputText, value: this.inputText});
+    }
 
     handleSelection(event){
         this.selectedOption = event.detail.value;
     }
 
     get options(){
-        return [
-            {label: 'Option 1', value: 1}, 
-            {label: 'Option 2', value: 2}, 
-            {label: 'Option 3', value: 3},
-            {label: 'Option 4', value: 4},
-        ]
+        return this.optionsArray
     }
 }
