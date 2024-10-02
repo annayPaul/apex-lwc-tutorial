4140e6697acc3ed3929c6b7be20b192ef1edf8a6
Annay Paul <paulannay14@gmail.com>
2024-10-02 14:10:24 +0600

    [Proto-1] Created a Component for the Dynamic Object Dropdown

--- force-app/main/default/lwc/dynamicObjectRecordDropdown/dynamicObjectRecordDropdown.js
+++ force-app/main/default/lwc/dynamicObjectRecordDropdown/dynamicObjectRecordDropdown.js
@@ -1,3 +1,55 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement, track, wire } from 'lwc';
+import getFields from "@salesforce/apex/Scratch.getFields";
 
-export default class DynamicObjectRecordDropdown extends LightningElement {}
+export default class DynamicObjectRecordDropdown extends LightningElement {
+  @api objectInput = "Account";
+  inputText = "";
+  selectedOption = "";
+  temp = [];
+
+  @track objectInfo;
+
+  @wire(getFields, {objectName: "$objectInput"})
+  objectInfo;
+
+  @track optionsArray = [
+    {label: "None", value: "None"}
+  ]
+
+  handleObjectInputChange(e){
+    // Can do debouncing!
+    this.objectInput = this.refs.objectSearch.value;
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    this.inputText = this.refs.searchInput.value;
+    this.optionsArray.push({label: this.inputText, value: this.inputText});
+    this.inputText = "";
+  }
+
+  handleObjectInputSubmit(e){
+    e.preventDefault();
+
+    this.objectInput = this.refs.objectSearch.value;
+    this.temp = [];
+
+    for(let field of this.objectInfo?.data){
+      this.temp.push({label: field, value: field})
+    }
+
+    this.optionsArray = [{label: "None", value: "None"}, ...this.temp];
+    this.objectInput = "";
+  }
+
+
+  handleSelection(event){
+    this.selectedOption = event.detail.value;
+  }
+
+
+  get options(){
+    return this.optionsArray;
+  }
+}
